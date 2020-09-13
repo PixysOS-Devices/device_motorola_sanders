2cc34f228e47b290eb23c6180bef872dd0a9099a
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    sanders: Add prebuilt protobuf from sdk28
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969
    Signed-off-by: xyyx <xyyx@mail.ru>

--- device.mk
+++ device.mk
@@ -336,6 +336,11 @@ PRODUCT_PACKAGES += charger_res_images
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
 
+# Protobuf
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so \
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so
+
 # Qualcomm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-oem.xml \
