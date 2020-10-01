bf05f9af4b4121d39f86270eb44045d76d3e50d8
lucasponez <lucasponez@outlook.com>
2020-10-01 16:04:21 +0000

    Revert "sanders: Nuke sepolicy for now"
    This reverts commit d4e4a715d5f0973537ab66b3c92724441c186b0c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -233,8 +233,8 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 VENDOR_SECURITY_PATCH := 2018-11-5
 
 # SELinux
-# include device/qcom/sepolicy-legacy-um/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
