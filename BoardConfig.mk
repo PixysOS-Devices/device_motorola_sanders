c60c81d74883a120cfe492e200a83c98829763c1
Lucas Ponez <lucasponez@outlook.com>
2020-10-13 11:45:12 -0300

    sanders: sepolicy: Open exception in a denial to fix Netflix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,6 +234,8 @@ VENDOR_SECURITY_PATCH := 2018-11-5
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
 
